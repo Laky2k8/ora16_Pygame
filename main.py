3e26c20fe2620afea0b7a564f2a035ef35332f20
Laky730 <benibubu2@gmail.com>
2021-05-28 18:04:19 +0200

    Movement and Background!

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pygame
 pygame.init()
 
 player = pygame.image.load("player.png")
+bg = pygame.image.load("sky.png")
 
-screen = pygame.display.set_mode([500,500])
+x = 450
+y = 200
+
+#player = pygame.transform.scale(player, (200,200))
+
+screen = pygame.display.set_mode([1000,500])
 pygame.display.set_caption("My First Pygame :D")
 
 running = True
@@ -15,8 +21,19 @@ while running:
             running = False
 
         screen.fill('white')
+        screen.blit(bg, (0, 0))
 
-        screen.blit(player, (200, 200))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                y -= 10
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                x -= 10
+            elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                y += 10
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                x += 10
+
+        screen.blit(player, (x, y))
         pygame.display.flip()
 
 pygame.quit()
