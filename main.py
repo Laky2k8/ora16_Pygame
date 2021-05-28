24e3656701dd76601696065a2c9c722b50f88fe9
Laky730 <benibubu2@gmail.com>
2021-05-28 17:25:45 +0200

    Drawing pictures :)

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ import pygame
 
 pygame.init()
 
+player = pygame.image.load("player.png")
+
+screen = pygame.display.set_mode([500,500])
+pygame.display.set_caption("My First Pygame :D")
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        screen.fill('white')
+
+        screen.blit(player, (200, 200))
+        pygame.display.flip()
+
+pygame.quit()
