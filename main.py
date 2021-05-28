2a5edab24d3b56c985ed492a6fb0ec9c6d538cf2
Laky730 <benibubu2@gmail.com>
2021-05-28 18:27:22 +0200

    Game v1.0 Done! Will update it tho :)

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import pygame
+import random
 
 pygame.init()
 
 player = pygame.image.load("player.png")
+enemy = pygame.image.load("enemy.png")
 bg = pygame.image.load("sky.png")
+
+collectible = pygame.image.load("collect.png")
 
 x = 450
 y = 200
+
+enemyX = 700
+enemyY = 100
+
+colX = random.randint(10, 500)
+colY = random.randint(10, 500)
+
+#enemy stuff
+steps_number = max( abs(enemyX-x), abs(enemyY-y) )
+
+
 
 #player = pygame.transform.scale(player, (200,200))
 
 screen = pygame.display.set_mode([1000,500])
 pygame.display.set_caption("My First Pygame :D")
+
+movingUp = False
 
 running = True
 while running:
@@ -22,6 +39,17 @@ while running:
 
         screen.fill('white')
         screen.blit(bg, (0, 0))
+
+        pRect = player.get_rect()
+        cRect = collectible.get_rect()
+
+
+
+        screen.blit(collectible, (colX, colY))
+
+
+
+        screen.blit(enemy, (enemyX, enemyY))
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == pygame.K_w:
@@ -34,6 +62,9 @@ while running:
                 x += 10
 
         screen.blit(player, (x, y))
+
+
+
         pygame.display.flip()
 
 pygame.quit()
